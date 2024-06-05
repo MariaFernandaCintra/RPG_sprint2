d572a784f46366a995bc022a1ff4a1d195a209b3
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2024-06-05 16:16:21 -0300

    Apresentação final - Sprint 2

--- Sprint_2/Sprint_2.ino
+++ Sprint_2/Sprint_2.ino
@@ -5,6 +5,7 @@ int NUMERO_DE_PARTIDAS = 5;
 int partida = 0;
 int GanhadorH = 0;
 int GanhadorV = 0;
+int Vitoria = 7;
 
 bool botaoPressionado1 = false;
 bool botaoPressionado2 = false;
@@ -20,25 +21,58 @@ bool botaoPressionado4 = false;
 #define ledVermelho 4
 //declarando a buzzer
 #define Buzzer 2
-//declarando as variaveis para limpar os estados dos botoes 
-int contH1 = 0;
-int contH2 = 0;
-int contV1 = 0;
-int contV2 = 0;
+
+// Definição das notas musicais
+#define NOTE_C4 262
+#define NOTE_D4 294
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_A4 440
+#define NOTE_B4 494
+#define NOTE_C5 523
+
+// Define a estrutura da melodia
+int melody[] = {
+  NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_F4, NOTE_E4,
+  NOTE_D4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4,
+  NOTE_E4, NOTE_D4, NOTE_D4, NOTE_C4, NOTE_C4,
+  NOTE_E4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_F4, NOTE_E4,
+  NOTE_D4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4,
+  NOTE_D4, NOTE_C4, NOTE_C4
+};
+
+// Define a duração de cada nota
+int noteDurations[] = {
+  4, 4, 4, 4, 4, 2, 4,
+  4, 4, 4, 4, 2,
+  4, 4, 4, 4, 2,
+  4, 4, 4, 4, 4, 2, 4,
+  4, 4, 4, 4, 2,
+  4, 4, 4
+};
 
 
 void setup() {
   Serial.begin(9600);
 
+  Serial.println(R"(
+ ==============================
+|     Bem-vindo ao jogo,       |
+|  Escolham seus personagens   |
+|      para o inicio da        |
+|         partida!!!           |
+ ==============================
+)");
+
   pinMode(BotaoHeroi1, INPUT);
   pinMode(BotaoVilao1, INPUT);
   pinMode(BotaoHeroi2, INPUT);
   pinMode(BotaoVilao2, INPUT);
-  pinMode(ledVerde, INPUT);
-  pinMode(ledVermelho, INPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
   pinMode(Buzzer, OUTPUT);
   delay(1000);
-
 }
 
 void limparEstado() {
@@ -49,10 +83,6 @@ void limparEstado() {
   Vilao2 = 0;
   PontosHerois = 0;
   PontosViloes = 0;
-  contH1 = 0;
-  contH2 = 0;
-  contV1 = 0;
-  contV2 = 0;
 }
 
 void limparEstadoBotao() {
@@ -65,20 +95,21 @@ void limparEstadoBotao() {
 
 
 void loop() {
+  //quem ganhou da partida
   String partida1 = "";
   String partida2 = "";
   String partida3 = "";
   String partida4 = "";
   String partida5 = "";
 
-  for (byte partida = 0; partida < NUMERO_DE_PARTIDAS;) {
+  for (byte partida = 0; partida < NUMERO_DE_PARTIDAS;) {//limitar as partidas
+    //lançamento de dados 
     if (!botaoPressionado1 && digitalRead(BotaoHeroi1) == 1) {
       Heroi1 = random(1, 7);
       Serial.print("Pontos do Heroi1: ");
       Serial.println(Heroi1);
       delay(1000);
       botaoPressionado1 = true;
-      contH1 += 1;
     }
 
     if (!botaoPressionado2 && digitalRead(BotaoHeroi2) == 1) {
@@ -87,7 +118,6 @@ void loop() {
       Serial.println(Heroi2);
       delay(1000);
       botaoPressionado2 = true;
-      contH2 += 1;
     }
 
     if (!botaoPressionado3 && digitalRead(BotaoVilao1) == 1) {
@@ -96,7 +126,6 @@ void loop() {
       Serial.println(Vilao1);
       delay(1000);
       botaoPressionado3 = true;
-      contV1 += 1;
     }
 
     if (!botaoPressionado4 && digitalRead(BotaoVilao2) == 1) {
@@ -105,28 +134,36 @@ void loop() {
       Serial.println(Vilao2);
       delay(1000);
       botaoPressionado4 = true;
-      contV2 += 1;
-    }
-    if (contH1 >= 2) {
-      Serial.println("Erro, voce clicou mais de uma vez no seu botao em uma unica partida");
     }
 
     PontosHerois = Heroi1 + Heroi2;
     PontosViloes = Vilao1 + Vilao2;
-    if (PontosHerois > 0 && PontosViloes > 0 && (Heroi1 != 0 && Heroi2 != 0 && Vilao1 != 0 && Vilao2 != 0)) {
-      if (PontosHerois > PontosViloes) {
-        Serial.print("Os herois venceram a partida!!!");
+    if (PontosHerois > 0 && PontosViloes > 0 && (Heroi1 != 0 && Heroi2 != 0 && Vilao1 != 0 && Vilao2 != 0 && partida < 5)) {
+      if (PontosHerois > PontosViloes && partida < 5) {
+        Serial.println("Os herois venceram a partida!!!");
         limparEstado();
         limparEstadoBotao();
-        delay(500);
-        digitalWrite(Buzzer, HIGH);
-        tone(Buzzer, 500);  // Frequência de 500Hz por 0.5 segundo caso os heróis ganh
         digitalWrite(ledVermelho, LOW);
-        //delay(1000);  // Mantém o LED verde ligado por 0.5 segundo
         digitalWrite(ledVerde, HIGH);
-        delay(2000);
+        tone(Buzzer, 663);
+        delay(400);
+        tone(Buzzer, 701);
+        delay(250);
+        tone(Buzzer, 589);
+        delay(400);
+        tone(Buzzer, 663);
+        delay(250);
+        tone(Buzzer, 526);
+        delay(400);
+        tone(Buzzer, 589);
+        delay(250);
+        tone(Buzzer, 440);
+        delay(1000);
+        noTone(Buzzer);
+        delay(1000);
         digitalWrite(ledVerde, LOW);
         noTone(Buzzer);
+        delay(1000);
         partida1 = "herois";
         GanhadorH += 1;
         partida2 = "herois";
@@ -137,19 +174,29 @@ void loop() {
         GanhadorH += 1;
         partida5 = "herois";
         GanhadorH += 1;
-      } 
-      else if (PontosViloes > PontosHerois) {
+      } else if (PontosViloes > PontosHerois && partida < 5) {
         Serial.println("Os viloes venceram a partida!!!");
         limparEstado();
         limparEstadoBotao();
-        delay(500);
-        tone(Buzzer, 1000);  // Frequência de 500Hz por 0.5 segundo caso os heróis ganh
         digitalWrite(ledVerde, LOW);
-        //delay(1000);  // Mantém o LED verde ligado por 0.5 segundo
         digitalWrite(ledVermelho, HIGH);
-        delay(2000);
-        digitalWrite(ledVermelho, LOW);
+        tone(Buzzer, 392);
+        delay(500);
+        tone(Buzzer, 440);
+        delay(250);
+        tone(Buzzer, 468);
+        delay(400);
+        tone(Buzzer, 589);
+        delay(400);
+        tone(Buzzer, 468);
+        delay(400);
+        tone(Buzzer, 440);
+        delay(250);
+        tone(Buzzer, 392);
+        delay(1000);
         noTone(Buzzer);
+        digitalWrite(ledVermelho, LOW);
+        delay(1000);
         partida1 = "viloes";
         GanhadorV += 1;
         partida2 = "viloes";
@@ -164,46 +211,20 @@ void loop() {
         Serial.println("Empate!!!");
         limparEstado();
         limparEstadoBotao();
-        tone(Buzzer, 750, 5000);  // Frequência de 750Hz por 5 segundo em caso de empate
-        digitalWrite(ledVermelho, HIGH);
-        delay(100);
-        digitalWrite(ledVerde, HIGH);
-        delay(100);
-        digitalWrite(ledVermelho, LOW);
-        delay(100);
-        digitalWrite(ledVerde, HIGH);
-        delay(100);
-        digitalWrite(ledVermelho, HIGH);
-        delay(100);
-        digitalWrite(ledVerde, LOW);
-        delay(100);
-        digitalWrite(ledVermelho, HIGH);
-        delay(100);
-        digitalWrite(ledVerde, HIGH);
-        delay(100);
-        digitalWrite(ledVermelho, LOW);
-        delay(100);
-        digitalWrite(ledVerde, HIGH);
-        delay(100);
-        digitalWrite(ledVermelho, HIGH);
-        delay(100);
-        digitalWrite(ledVerde, LOW);
-        delay(100);
-        digitalWrite(ledVermelho, HIGH);
-        delay(100);
-        digitalWrite(ledVerde, HIGH);
-        delay(100);
-        digitalWrite(ledVermelho, LOW);
-        delay(100);
-        digitalWrite(ledVerde, HIGH);
-        delay(100);
-        digitalWrite(ledVermelho, HIGH);
-        delay(100);
-        digitalWrite(ledVerde, LOW);
-        delay(100);
-        digitalWrite(ledVermelho, LOW);
-        delay(100);
-        noTone(Buzzer);
+        for (byte i = 0; i < Vitoria; i++) {
+          tone(Buzzer, 750, 5000);  // Frequência de 750Hz por 5 segundo em caso de empate
+          digitalWrite(ledVermelho, HIGH);
+          delay(100);
+          digitalWrite(ledVerde, HIGH);
+          noTone(Buzzer);
+          delay(250);
+          tone(Buzzer, 750);
+          digitalWrite(ledVermelho, LOW);
+          delay(100);
+          digitalWrite(ledVerde, LOW);
+          noTone(Buzzer);
+          delay(250);
+        }
       }
       partida += 1;
       Serial.print("Partida ");
@@ -213,27 +234,32 @@ void loop() {
 
     if (partida == 5) {
       if (GanhadorH > GanhadorV) {
-        Serial.println("Os herois venceram o jogo"); 
-        for (byte i = 0; i < 10; i++){
-          tone(Buzzer, 500);
+        Serial.println("Os herois venceram o jogo");
+        // Reproduz a melodia
+        for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++) {
+          int duration = 1000 / noteDurations[i];
           digitalWrite(ledVerde, HIGH);
-          delay(50);
-          noTone(Buzzer);
+          tone(Buzzer, melody[i], duration);
+          delay(duration + 50);  // Adiciona um pequeno atraso entre cada nota
           digitalWrite(ledVerde, LOW);
-          delay(100);
-        }    
+        }
       }
 
       if (GanhadorV > GanhadorH) {
         Serial.println("Os viloes venceram o jogo");
-        for (byte i2 = 0; i2 < 10; i2++){
-          tone(Buzzer, 1000);
+        // Reproduz a melodia
+        for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++) {
+          int duration = 1000 / noteDurations[i];
           digitalWrite(ledVermelho, HIGH);
-          delay(50);
-          noTone(Buzzer);
+          tone(Buzzer, melody[i], duration);
+          delay(duration + 50);
           digitalWrite(ledVermelho, LOW);
-          delay(100);
-        }    
+          
+        }
+      }
+
+      else if (GanhadorV == GanhadorH) {
+        Serial.println("O JOGO TERMINOU EMPATADO!!!");
       }
       Serial.println("Fim de jogo");
     }
